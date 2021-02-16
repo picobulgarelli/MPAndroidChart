067b836bca311b8c8029489540cd0be1eb92c7ac
picobulgarelli <bulgarelli@picotronik.it>
2021-02-16 16:48:04 +0100

    * Added multiple entries in realtime

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/RealtimeLineChartActivity.java
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/RealtimeLineChartActivity.java
@@ -29,6 +29,10 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RealtimeLineChartActivity extends DemoBase implements
         OnChartValueSelectedListener {
 
@@ -61,7 +65,7 @@ public class RealtimeLineChartActivity extends DemoBase implements
         chart.setPinchZoom(true);
 
         // set an alternative background color
-        chart.setBackgroundColor(Color.LTGRAY);
+        chart.setBackgroundColor(Color.BLACK);
 
         LineData data = new LineData();
         data.setValueTextColor(Color.WHITE);
@@ -81,18 +85,31 @@ public class RealtimeLineChartActivity extends DemoBase implements
         xl.setTypeface(tfLight);
         xl.setTextColor(Color.WHITE);
         xl.setDrawGridLines(false);
+        xl.setDrawLabels(true);
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
         xl.setAvoidFirstLastClipping(true);
+        //xl.setAxisMinimum(0f);
+        //xl.setAxisMaximum(24f);
         xl.setEnabled(true);
 
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setTypeface(tfLight);
-        leftAxis.setTextColor(Color.WHITE);
-        leftAxis.setAxisMaximum(100f);
-        leftAxis.setAxisMinimum(0f);
-        leftAxis.setDrawGridLines(true);
+        leftAxis.setTextColor(Color.CYAN);
+        leftAxis.setAxisLineColor(Color.CYAN);
+        leftAxis.setAxisMaximum(20f);
+        leftAxis.setAxisMinimum(-30f);
+        leftAxis.setDrawLabels(true);
+        leftAxis.setDrawGridLines(false);
 
         YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
+        rightAxis.setTypeface(tfLight);
+        rightAxis.setTextColor(Color.RED);
+        rightAxis.setAxisLineColor(Color.RED);
+        rightAxis.setAxisMaximum(20f);
+        rightAxis.setAxisMinimum(-30f);
+        rightAxis.setDrawLabels(true);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setEnabled(true);
 
     }
 
@@ -106,11 +123,11 @@ public class RealtimeLineChartActivity extends DemoBase implements
             // set.addEntry(...); // can be called as well
 
             if (set == null) {
-                set = createSet();
+                set = createSet(ColorTemplate.getHoloBlue(), "Pressione Minima");
                 data.addDataSet(set);
             }
 
-            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), (float) new Random().nextInt(4 + 0) - 20), 0);//(Math.random() * 40) + 30f), 0);
             data.notifyDataChanged();
 
             // let the chart know it's data has changed
@@ -129,16 +146,109 @@ public class RealtimeLineChartActivity extends DemoBase implements
         }
     }
 
-    private LineDataSet createSet() {
+    private void addEntries() {
+        LineData data = chart.getData();
 
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            ILineDataSet setOther = data.getDataSetByIndex(1);
+
+            if (set == null || setOther == null) {
+                set = createSet(ColorTemplate.getHoloBlue(), "Pressione Minima [CmH2O]");
+                data.addDataSet(set);
+
+                setOther = createSet(Color.RED, "Pressione Massima [CmH2O]");
+                data.addDataSet(setOther);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount() / 10f, (float) new Random().nextInt(5 + 30) - 30), 0);
+            data.addEntry(new Entry(setOther.getEntryCount() / 10f, (float) new Random().nextInt(15 + 30) - 30), 1);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            chart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            chart.setVisibleXRangeMaximum(24);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            chart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    private void addMultipleEntries() {
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+        String[] name = new String[] {
+                "Pressione Minima",
+                "Pressione Massima"
+        };
+
+        for (int z = 0; z < 2; z++) {
+
+            ArrayList<Entry> values = new ArrayList<>();
+
+            for (int i = 0; i < 80; i++) {
+                float val;
+                if (z == 0) {
+                    val = new Random().nextInt(4 + 20) - 20;
+                } else {
+                    val = new Random().nextInt(4 + 8) - 8;
+                }
+                values.add(new Entry(i, val));
+            }
+
+            LineDataSet d = new LineDataSet(values, name[z]);
+            d.setLineWidth(2f);
+            d.setCircleRadius(2f);
+
+            int[] colors = new int[] {
+                    ColorTemplate.getHoloBlue(),
+                    Color.RED
+            };
+            d.setColor(colors[z]);
+            d.setCircleColor(Color.WHITE);
+            d.setFillAlpha(65);
+            d.setFillColor(colors[z]);
+            d.setHighLightColor(Color.rgb(244, 117, 117));
+            d.setValueTextColor(Color.WHITE);
+            d.setValueTextSize(9f);
+            d.setDrawValues(false);
+            if (z != 0) {
+                d.setAxisDependency(AxisDependency.RIGHT);
+            } else {
+                d.setAxisDependency(AxisDependency.LEFT);
+            }
+            d.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            dataSets.add(d);
+        }
+
+        // make the first DataSet dashed
+        //((LineDataSet) dataSets.get(0)).enableDashedLine(10, 10, 0);
+        //((LineDataSet) dataSets.get(0)).setColors(ColorTemplate.VORDIPLOM_COLORS);
+        //((LineDataSet) dataSets.get(0)).setCircleColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        LineData data = new LineData(dataSets);
+        chart.setData(data);
+        // limit the number of visible entries
+        chart.setVisibleXRangeMaximum(120);
+        chart.invalidate();
+    }
+
+    private LineDataSet createSet(int color, String name) {
+
+        LineDataSet set = new LineDataSet(null, name);
         set.setAxisDependency(AxisDependency.LEFT);
-        set.setColor(ColorTemplate.getHoloBlue());
+        set.setColor(color);
         set.setCircleColor(Color.WHITE);
         set.setLineWidth(2f);
-        set.setCircleRadius(4f);
+        set.setCircleRadius(2f);
         set.setFillAlpha(65);
-        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setFillColor(color);
         set.setHighLightColor(Color.rgb(244, 117, 117));
         set.setValueTextColor(Color.WHITE);
         set.setValueTextSize(9f);
@@ -157,7 +267,8 @@ public class RealtimeLineChartActivity extends DemoBase implements
 
             @Override
             public void run() {
-                addEntry();
+                addEntries();
+                //addMultipleEntries();
             }
         };
 
@@ -180,6 +291,7 @@ public class RealtimeLineChartActivity extends DemoBase implements
         });
 
         thread.start();
+
     }
 
     @Override
@@ -209,6 +321,20 @@ public class RealtimeLineChartActivity extends DemoBase implements
             }
             case R.id.actionFeedMultiple: {
                 feedMultiple();
+                break;
+            }
+            case R.id.actionToggleCubic: {
+                List<ILineDataSet> sets = chart.getData()
+                        .getDataSets();
+
+                for (ILineDataSet iSet : sets) {
+
+                    LineDataSet set = (LineDataSet) iSet;
+                    set.setMode(set.getMode() == LineDataSet.Mode.CUBIC_BEZIER
+                            ? LineDataSet.Mode.LINEAR
+                            :  LineDataSet.Mode.CUBIC_BEZIER);
+                }
+                chart.invalidate();
                 break;
             }
             case R.id.actionSave: {
